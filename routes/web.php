bb019a8bcd250feac67af6f7fd6e7c8ebdbc3a0a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-30 17:10:07 +0100

    added routes sneaker-edit and sneaker-update in web.php

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/sneakers', [SneakerController::class, "index"])->name("sneaker-inde
 Route::get('/sneakers/create', [SneakerController::class, "create"])->name("sneaker-create");
 Route::get('/sneakers/{id}', [SneakerController::class, "show"])->name("sneaker-show");
 Route::post('/sneakers', [SneakerController::class, "store"])->name("sneaker-store");
+Route::get('/sneakers/{id}/edit', [SneakerController::class, "edit"])->name("sneaker-edit");
+Route::put('/sneakers/{id}', [SneakerController::class, "update"])->name("sneaker-update");
